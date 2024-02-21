fa322d5a9b462de75030422472a40486890a3011
JD Tadlock <jt7903@gmail.com>
2024-02-21 14:15:29 -0500

    finished basic conditional page rendering - awful

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
+import { useState } from 'react'
+
 import Header from './components/Header'
+// Import the Home component
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+import About from './pages/About'
 
 function App() {
-  const title = 'React Overview'
-  const showTitle = true
+  const [page, setPage] = useState('home')
+
+  function renderPage() {
+    switch (page) {
+      case 'home':
+        return <Home />
+      case 'about':
+        return <About />
+      default:
+        return <Contact />
+    }
+  }
 
   return (
     <>
-
-      <Header mainTitle={title} />
+      <Header page={page} setPage={setPage} />
       <main>
-        {showTitle && <h1>{title}</h1>}
-
-        <p>{5 > 3 ? 'Yep, 5 is greater' : 'Nope, math is not true'}</p>
+        {/* Conditionally output the Home component if page is set to 'home', likewise output Contact if it's equal to 'contact' and About if equal to 'about' */}
+        {/* {page === 'home' ? <Home /> :
+          page === 'contact' ? <Contact /> : <About />} */}
+        {renderPage()}
       </main>
     </>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,19 @@
 function Header(props) {
-  console.log(props);
+  const handleLinkClick = e => {
+    e.preventDefault();
+
+    props.setPage(e.target.innerText.toLowerCase());
+  }
+
   return (
     <header>
-      <h3>{props.mainTitle}</h3>
+      <h3>Portfolio Practice</h3>
+
+      <nav>
+        <a onClick={handleLinkClick} href="/" className={props.page === 'home' ? 'active' : ''}>Home</a>
+        <a onClick={handleLinkClick} href="/about" className={props.page === 'about' ? 'active' : ''}>About</a>
+        <a onClick={handleLinkClick} href="/contact" className={props.page === 'contact' ? 'active' : ''}>Contact</a>
+      </nav>
     </header>
   )
 }
